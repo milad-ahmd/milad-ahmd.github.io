dfbd6bd1eba366d65644d0b0bf65edd0c1076a4f
milad ahmadi <miladahmadi803@gmail.com>
2019-12-31 13:13:35 +0330

    build

---
+++ precache-manifest.e635bf8d1bc0fdf5e02b3df850cd4aa1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c47d6e574d2201566df54a96a16a8ba1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1cb9483f81ca3176cc37",
+    "url": "/static/css/main.5290285c.chunk.css"
+  },
+  {
+    "revision": "09eee842a98fc157c1ca",
+    "url": "/static/js/2.c01b1aee.chunk.js"
+  },
+  {
+    "revision": "e6edf5f5109ad7460f75a9f5716d868f",
+    "url": "/static/js/2.c01b1aee.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1cb9483f81ca3176cc37",
+    "url": "/static/js/main.7c68bd51.chunk.js"
+  },
+  {
+    "revision": "afe0e8010d3c4acfd96c",
+    "url": "/static/js/runtime-main.20a96f20.js"
+  },
+  {
+    "revision": "e62f3ae952e61e2da4ea85d7717f89c7",
+    "url": "/static/media/logo.e62f3ae9.jpg"
+  },
+  {
+    "revision": "d4396cb23ddbdaca1ae29a1d04748a32",
+    "url": "/static/media/topSlider1.d4396cb2.jpg"
+  }
+]);
